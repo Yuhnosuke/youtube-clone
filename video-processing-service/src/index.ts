b00f7269e9881012b09b1bf4ef8a0070f3b0eb00
Yuhnosuke <h.yuunosuke@gmail.com>
2025-07-30 17:02:08 +0900

    process videos locally

--- video-processing-service/src/index.ts
+++ video-processing-service/src/index.ts
@@ -1,12 +1,36 @@
 import express from 'express'
+import ffmpeg from 'fluent-ffmpeg'
 
 const app = express()
-const port = 3000
+app.use(express.json())
 
-app.get('/', (req, res) => {
-  res.send('Hello, World!')
+app.post('/process-video', (req, res) => {
+  // Get the path of the input video file from the request body
+  const inputFilePath = req.body.inputFilePath
+  const outputFilePath = req.body.outputFilePath
+
+  // Check if the input file path is defined
+  if (!inputFilePath || !outputFilePath) {
+    return res.status(400).send('Bad Request: Missing file path')
+  }
+
+  // Create the ffmpeg command
+  ffmpeg(inputFilePath)
+    // .videoCodec('libx264')
+    // .audioCodec('aac')
+    .outputOptions('-vf', 'scale=-2:360')
+    .on('end', () => {
+      console.log('Processing finished successfully')
+      res.status(200).send('Processing finished successfully')
+    })
+    .on('error', (err) => {
+      console.log('An error occurred: ' + err.message)
+      res.status(500).send('An error occurred: ' + err.message)
+    })
+    .save(outputFilePath)
 })
 
+const port = process.env.PORT || 3000
 app.listen(port, () => {
-  console.log(`Video Proceccing Service is listening on http://localhost:${port}`)
+  console.log(`Server is running on port ${port}`)
 })
