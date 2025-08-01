054094427643cada2bf4d554102b6479ccf9a928
Yuhnosuke <h.yuunosuke@gmail.com>
2025-08-01 13:25:50 +0900

    setup Cloud Run, Cloud Storage, and Pub/Sub

--- video-processing-service/src/index.ts
+++ video-processing-service/src/index.ts
@@ -1,33 +1,63 @@
 import express from 'express'
-import ffmpeg from 'fluent-ffmpeg'
+import {
+  uploadProcessedVideo,
+  downloadRawVideo,
+  deleteProcessedVideo,
+  deleteRawVideo,
+  convertVideo,
+  setupDirectories,
+  // doesFileExist,
+} from './storage'
+
+setupDirectories()
 
 const app = express()
 app.use(express.json())
 
-app.post('/process-video', (req, res) => {
-  // Get the path of the input video file from the request body
-  const inputFilePath = req.body.inputFilePath
-  const outputFilePath = req.body.outputFilePath
-
-  // Check if the input file path is defined
-  if (!inputFilePath || !outputFilePath) {
-    return res.status(400).send('Bad Request: Missing file path')
+app.post('/process-video', async (req, res) => {
+  // Get the bucket and filename from the Cloud Pub/Sub message
+  let data
+  try {
+    const message = Buffer.from(req.body.message.data, 'base64').toString('utf8')
+    data = JSON.parse(message)
+    if (!data.name) {
+      throw new Error('Invalid message payload received.')
+    }
+  } catch (error) {
+    console.error(error)
+    return res.status(400).send('Bad Request: missing filename.')
   }
 
-  // Create the ffmpeg command
-  ffmpeg(inputFilePath)
-    // .videoCodec('libx264')
-    // .audioCodec('aac')
-    .outputOptions('-vf', 'scale=-2:360')
-    .on('end', () => {
-      console.log('Processing finished successfully')
-      res.status(200).send('Processing finished successfully')
-    })
-    .on('error', (err) => {
-      console.log('An error occurred: ' + err.message)
-      res.status(500).send('An error occurred: ' + err.message)
-    })
-    .save(outputFilePath)
+  const inputFileName = data.name
+  const outputFileName = `processed-${inputFileName}`
+
+  // const rawVideoBucketName = process.env.RAW_VIDEO_BUCKET_NAME
+  // if (!rawVideoBucketName) {
+  //   return res.status(500).send('Missing RAW_VIDEO_BUCKET_NAME environment variable')
+  // }
+
+  // const exists = await doesFileExist(rawVideoBucketName, inputFileName)
+  // if (!exists) {
+  //   return res.status(404).send(`File not found: ${inputFileName}`)
+  // }
+
+  // Download the raw video from Cloud Storage
+  await downloadRawVideo(inputFileName)
+
+  // Process the video into 360p
+  try {
+    await convertVideo(inputFileName, outputFileName)
+  } catch (err) {
+    await Promise.all([deleteRawVideo(inputFileName), deleteProcessedVideo(outputFileName)])
+    return res.status(500).send('Processing failed')
+  }
+
+  // Upload the processed video to Cloud Storage
+  await uploadProcessedVideo(outputFileName)
+
+  await Promise.all([deleteRawVideo(inputFileName), deleteProcessedVideo(outputFileName)])
+
+  return res.status(200).send('Processing finished successfully')
 })
 
 const port = process.env.PORT || 3000
